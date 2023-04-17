837c061d2cb5e3ad91080a316d7618c44d4ef7e3
vpnchengo <charo.alvin@gmail.com>
2023-04-17 14:44:15 +0000

    mysql test db setup

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,6 +3,6 @@
 -- A new user hbnb_test (in localhost) will all privileges
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_dev'@'localhost';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';

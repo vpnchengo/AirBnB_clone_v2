e26b7131271450294b949c5f1a874ad3e0ba07a8
vpnchengo <charo.alvin@gmail.com>
2023-04-17 14:36:18 +0000

    MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- Script that creates a MySQL server for the project
+-- A database hbnb_test_db
+-- A new user hbnb_test (in localhost) will all privileges
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';

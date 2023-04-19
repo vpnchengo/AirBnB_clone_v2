8f490bd54f454c25250dfbc750733f9fc1e96724
vpnchengo <charo.alvin@gmail.com>
2023-04-19 08:53:13 +0000

    getter attribute update

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, Foreignkey
 from sqlalchemy.orm import relationship
+from models.city import City
 
 
 class State(BaseModel):
@@ -12,3 +13,16 @@ class State(BaseModel):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state",
                           cascade="all, delete, delete-orphan")
+
+    @property
+    def cities(self):
+        """getter attribute cities that returns the list of City instances
+        with state_id equals to the current State.id =>
+        It will be the FileStorage relationship between State and City
+        """
+        mylist = []
+        extracted_result = models.storage.all(City).values()
+        for city in extracted_result:
+            if self.id == city.state_id:
+                mylist.append(city)
+        return mylist

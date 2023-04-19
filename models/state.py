c44b634210196b38238b02b029aa8cd7b91f1f32
vpnchengo <charo.alvin@gmail.com>
2023-04-19 00:14:18 +0000

    updated state attributes

--- models/state.py
+++ models/state.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Foreignkey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete, delete-orphan")

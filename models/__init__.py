b28045532bdf9d1efede45600f11c06eda0e6f0b
vpnchengo <charo.alvin@gmail.com>
2023-04-19 09:21:06 +0000

    added a condition depending on env. var. value

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of class FileStorage
+    If environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
+    instantiates DBStorage
+    else, instatiates FileStorage
+"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from os import getenv
 
 
-storage = FileStorage()
+if os.getenv('HBNB_TYPE_STORAGE') = 'db':
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()

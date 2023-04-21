6aa0fa1a43474ac92881d0adf891dd369333ba71
vpnchengo <charo.alvin@gmail.com>
2023-04-21 09:15:08 +0000

    import update

--- console.py
+++ console.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel
 from models.state import State
 from models.amenity import Amenity
 from models.review import Review
-from models import storage
+from models.__init__ import stoarge
 from models.user import User
 from models.place import Place
 from models.city import City

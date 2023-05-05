760409334e7e30de4dd1fc09fde639cad0f1f8b2
vpnchengo <charo.alvin@gmail.com>
2023-05-05 13:38:00 +0000

    preparing web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# a Bash script that sets up your web servers for the deployment of web_static
+
+sudo apt-get update
+
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "<!DOCTYPE html>
+  <head>
+  </head>
+  <body>  	
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i "/listen 80 default_server/a location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+
+sudo service nginx restart
